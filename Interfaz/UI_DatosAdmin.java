a49fb45261dd58aaa0efbde23aaa262c9a516323
javier garzas <javier.garzas@alu.uclm.es>
2024-10-17 18:23:44 +0200

    Clase UI_DatosAdmin.java-interfaz

---
+++ Interfaz/UI_DatosAdmin.java
@@ -0,0 +1,14 @@
+public class UI_DatosAdmin {
+
+	public void introducirSolicitud(String aSolicitud) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void verHistorialSolicitudes() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarResultado(Respuesta aResultado) {
+		throw new UnsupportedOperationException();
+	}
+}
