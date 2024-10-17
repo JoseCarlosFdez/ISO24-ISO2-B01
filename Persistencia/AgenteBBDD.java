0096dcfee0310e47f7a296525d6622b20d109fda
javier garzas <javier.garzas@alu.uclm.es>
2024-10-17 18:52:10 +0200

    clase agente junto con la carpeta persistencia

---
+++ Persistencia/AgenteBBDD.java
@@ -0,0 +1,24 @@
+public class AgenteBBDD {
+	private AgenteBBDD _instancia;
+	private MySqlConnection _conexion;
+
+	public AgenteBBDD getAgente() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Lista select() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void cambiar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void conectar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void desconectar() {
+		throw new UnsupportedOperationException();
+	}
+}
