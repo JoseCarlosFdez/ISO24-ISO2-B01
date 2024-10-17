c8a9b7daaddbde428ce710e1018a9ec7c48e0486
javier garzas <javier.garzas@alu.uclm.es>
2024-10-17 19:11:09 +0200

    Carpeta Dominio jnto con las clases Usuario_Admin y UsuarioAdminDAO

---
+++ Dominio/UsuarioAdminDAO.java
@@ -0,0 +1,22 @@
+public class UsuarioAdminDAO {
+
+	public void select(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void selectAll(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void insert(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void delete(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void update(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ Dominio/Usuario_Admin.java
@@ -0,0 +1,21 @@
+public class Usuario_Admin {
+	private String _nombre;
+	private int _idUsuario;
+	private String _correo;
+
+	public void introducirSolicitud(String aSolicitud) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getnombre() {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getcorreo() {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Solicitud> verHistorialSolicitudes() {
+		throw new UnsupportedOperationException();
+	}
+}
