37225f0be93be4375e247ddce89237c1e4c2f5f3
Carlos-SD4 <146247295+Carlos-SD4@users.noreply.github.com>
2024-11-05 14:58:13 +0100

    V0.4

---
+++ Dominio/DefinirDominio/Técnico.java
@@ -0,0 +1,13 @@
+public class Técnico {
+
+	public void identificarRequisitosDatos() {
+		// TODO - implement Técnico.identificarRequisitosDatos
+		throw new UnsupportedOperationException();
+	}
+
+	public void documentarRequisito() {
+		// TODO - implement Técnico.documentarRequisito
+		throw new UnsupportedOperationException();
+	}
+
+}
