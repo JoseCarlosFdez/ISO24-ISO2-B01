0602c68c6635592a247dcfa5ca2323fae622bef8
javier-pardo4 <146756836+javier-pardo4@users.noreply.github.com>
2024-11-12 21:57:34 +0100

    Upload Permiso.java

---
+++ Dominio/DefinirDominio/Permiso.java
@@ -0,0 +1,23 @@
+public class Permiso {
+
+	private int ID;
+	private int Directorio;
+	private int Función;
+	private int Descripcion;
+
+	public void AsignarDirectorio() {
+		// TODO - implement Permiso.AsignarDirectorio
+		throw new UnsupportedOperationException();
+	}
+
+	public void RevocarPermiso() {
+		// TODO - implement Permiso.RevocarPermiso
+		throw new UnsupportedOperationException();
+	}
+
+	public void MostrarUsuarios() {
+		// TODO - implement Permiso.MostrarUsuarios
+		throw new UnsupportedOperationException();
+	}
+
+}
