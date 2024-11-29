bbd194e1a805b16c60ae9b888ff1e1117ca11da3
Dmedina-coder <daniel.medina4@alu.uclm.es>
2024-11-29 18:16:47 +0100

    Subido el componente 6 del servidor

---
+++ Server/src/main/java/ISOB01/Server/Alerta.java
@@ -0,0 +1,13 @@
+public class Alerta {
+
+	private int id;
+	private int mensaje;
+	private int tipo;
+	private int fecha;
+
+	public void enviar() {
+		// TODO - implement Alerta.enviar
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Server/src/main/java/ISOB01/Server/SistemaDeNotificaciones.java
@@ -0,0 +1,16 @@
+public class SistemaDeNotificaciones {
+
+	private int usuarios;
+	private int alertas;
+
+	public void crearAlerta() {
+		// TODO - implement SistemaDeNotificaciones.crearAlerta
+		throw new UnsupportedOperationException();
+	}
+
+	public void enviarNotificaciones() {
+		// TODO - implement SistemaDeNotificaciones.enviarNotificaciones
+		throw new UnsupportedOperationException();
+	}
+
+}
