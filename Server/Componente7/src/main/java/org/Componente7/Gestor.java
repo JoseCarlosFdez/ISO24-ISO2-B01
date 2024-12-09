edfbdd616a6a09930dc7239fcc6427dbb4debe80
José Carlos Fernández Verdejo <160581844+JoseCarlosFdez@users.noreply.github.com>
2024-12-09 18:24:13 +0100

    Implementacion Servidor

---
+++ Server/Componente7/src/main/java/org/Componente7/Gestor.java
@@ -0,0 +1,23 @@
+public class Gestor {
+
+	public void GuardarConexion() {
+		// TODO - implement Gestor.GuardarConexion
+		throw new UnsupportedOperationException();
+	}
+
+	public void EditarConexion() {
+		// TODO - implement Gestor.EditarConexion
+		throw new UnsupportedOperationException();
+	}
+
+	public void CerrarConexion() {
+		// TODO - implement Gestor.CerrarConexion
+		throw new UnsupportedOperationException();
+	}
+
+	public void ejecutarConsultas() {
+		// TODO - implement Gestor.ejecutarConsultas
+		throw new UnsupportedOperationException();
+	}
+
+}
