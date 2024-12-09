7c6a93ac5870120dceb0f020220a6a80246f04e6
José Carlos Fernández Verdejo <160581844+JoseCarlosFdez@users.noreply.github.com>
2024-12-09 18:20:56 +0100

    Implementacion Cliente

---
+++ Cliente/Componente7/src/main/java/org/Componente7/cliente.java
@@ -0,0 +1,18 @@
+public class cliente {
+
+	public void ConectarBBDD() {
+		// TODO - implement cliente.ConectarBBDD
+		throw new UnsupportedOperationException();
+	}
+
+	public void Navegar() {
+		// TODO - implement cliente.Navegar
+		throw new UnsupportedOperationException();
+	}
+
+	public void VisualizarResoltados() {
+		// TODO - implement cliente.VisualizarResoltados
+		throw new UnsupportedOperationException();
+	}
+
+}
